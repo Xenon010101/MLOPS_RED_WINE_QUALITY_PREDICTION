b312dc9efdf001208ecc627293bfd9c55deb9c43
kallappa2005 <kallappakabbur874@gmail.com>
2026-01-24 21:30:39 +0530

    data validation added

---
+++ src/mlProject/pipeline/stage_02_data_validation.py
@@ -0,0 +1,18 @@
+from mlProject.components.data_validation import DataValidation
+from mlProject.config.configuration import ConfigurationManager
+from mlProject import logger
+
+
+STAGE_NAME = "Data Validation Stage"
+
+class DataValidationTrainingPipeline:
+    def __init_(self):
+        pass
+
+    def main(self):
+        config = ConfigurationManager()
+        data_validation_config = config.get_data_validation_config()
+        data_validation = DataValidation(config=data_validation_config)
+        data_validation.validate_all_columns()
+
+        
